e56db7c7dcd65e2e150612201be74d2d09a160d8
Arie De Bok <arie@directdebit.co.za>
2022-08-03 12:07:07 +0200

    Add an example on how to override the style

--- frontend/src/components/PCDForm.vue
+++ frontend/src/components/PCDForm.vue
@@ -13,6 +13,18 @@ onMounted(async () => {
     submit_secret: submit_info["secret"],
     submit_url: submit_info["url"],
   })
+
+  // If you want to change the way submit button looks, you can select it by id and just update it with raw javascript.
+  // Uncomment the code below to see how it works
+  /*
+   let submitButton = document.getElementById("pcivault-pcd-form-button-submit")
+   submitButton.style.background = "blue"
+   submitButton.innerHTML = "Submit"
+
+   // you can even add and remove classes to completely override the default styling.
+   submitButton.classList.remove("card-form__button")
+   submitButton.classList.add("custom_style")
+   */
 })
 </script>
 
@@ -21,3 +33,10 @@ export default {
   name: "PCDForm",
 }
 </script>
+
+<style>
+.custom_style {
+  padding: 25px;
+  color: red;
+}
+</style>
